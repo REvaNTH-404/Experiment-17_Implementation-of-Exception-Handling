774c990bd8528d3f446693a87755f2247a4b016a
Revanth Sai Sreerangam <revanthsai.work@gmail.com>
2025-09-28 08:56:41 +0530

    Add files via upload

---
+++ EXP16-a.cpp
@@ -0,0 +1,26 @@
+// Harsh kumar
+// 24070123146
+// B3
+#include <iostream>
+int main(){
+    float n1,n2,ans;
+    std:: cout<<"enter value of number 1 & 2: ";
+    std::cin>>n1>>n2;
+    try{
+        if(n2==0){
+            throw n2;
+        }
+        else{
+            ans=n1/n2;
+            std::cout<<"answer = " <<ans<<"\n";
+        }
+    }
+    catch(float num){
+        std::cout << "\nERROR : division by " << num << "\n";
+    }
+}
+
+/* Output:
+enter value of number 1 & 2: 12 35
+answer = 0.342857
+*/
